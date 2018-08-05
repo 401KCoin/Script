20a2d878ff184f70e288c6dce9309d3efc36c23e
401KCoin <40668199+401KCoin@users.noreply.github.com>
2018-08-05 16:25:42 +0800

    Update setup.sh
    remove swap feature

--- setup.sh
+++ setup.sh
@@ -33,16 +33,6 @@ if [[ $DOSETUP =~ "y" ]] ; then
   sudo apt-get update
   sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 
-  cd /var
-  sudo touch swap.img
-  sudo chmod 600 swap.img
-  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-  sudo mkswap /var/swap.img
-  sudo swapon /var/swap.img
-  sudo free
-  sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
-  cd
-
   sudo apt-get install -y ufw
   sudo ufw allow ssh/tcp
   sudo ufw limit ssh/tcp
